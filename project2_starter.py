7290179c78c1173157eb3f38d76da51995a80ba6
Tiffany Sudijono <tiffanysudijono@227-3.synergydsl.com>
2026-03-27 18:53:37 -0400

    Finished test_create_listing_database Function

--- project2_starter.py
+++ project2_starter.py
@@ -404,7 +404,13 @@ class TestCases(unittest.TestCase):
         # (listing_title, listing_id, policy_number, host_type, host_name, room_type, location_rating)
 
         # TODO: Spot-check the LAST tuple is ("Guest suite in Mission District", "467507", "STR-0005349", "Superhost", "Jennifer", "Entire Room", 4.8).
-        pass
+        for row in self.detailed_data:
+            self.assertEqual(len(row), 7)
+
+        self.assertEqual(
+            self.detailed_data[-1],
+            ("Guest suite in Mission District", "467507", "STR-0005349", "Superhost", "Jennifer", "Entire Room", 4.8)
+        )
 
     def test_output_csv(self):
         out_path = os.path.join(self.base_dir, "test.csv")
